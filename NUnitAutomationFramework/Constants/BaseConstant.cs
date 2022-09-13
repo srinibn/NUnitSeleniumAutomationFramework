0944830f2233ee443bc857beb8942aa07877cb4c
srinivas <srinivasbommakanti225@gmail.com>
2022-09-13 13:55:34 +0530

    Pushing constant variable

--- NUnitAutomationFramework/Constants/BaseConstant.cs
+++ NUnitAutomationFramework/Constants/BaseConstant.cs
@@ -9,5 +9,6 @@ namespace NUnitAutomationFramework.Constants
     public class BaseConstant
     {
         public static string testdata = "Testing";
+        public static string test = "Testing";
     }
 }
